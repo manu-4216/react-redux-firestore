9204b90afdf689f6dc506c341e824b758931488f
Manuel Micu <manu4216@gmail.com>
2018-07-25 23:04:47 +0200

    Add static EventForm

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
+import EventForm from '../EventForm/EventForm';
 
 class EventDashboard extends Component {
-    render() {
-        return (
-            <Grid>
-                <Grid.Column width={10}>
-                    <EventList />
-                </Grid.Column>
+  render() {
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList />
+        </Grid.Column>
 
-                <Grid.Column width={6}>
-                    <h2>Right Column</h2>
-                </Grid.Column>
-            </Grid>
-        );
-    }
+        <Grid.Column width={6}>
+          <Button positive content="Create Event" />
+          <EventForm />
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default EventDashboard;
