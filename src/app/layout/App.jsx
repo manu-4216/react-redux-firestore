6eba483302a00f363592adb2d25a6bcde876a078
manu-4216 <manu4216@gmail.com>
2018-08-08 23:37:07 +0200

    Add router links

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import { Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
+import HomePage from '../../features/home/HomePage';
 
 class App extends Component {
   render() {
@@ -9,7 +16,13 @@ class App extends Component {
       <Fragment>
         <NavBar />
         <Container className="main">
-          <EventDashboard />
+          <Route exact path="/" strict component={HomePage} />
+          <Route path="/events" component={EventDashboard} />
+          <Route path="/event/:id" component={EventDetailedPage} />
+          <Route path="/people" component={PeopleDashboard} />
+          <Route path="/profile/:id" component={UserDetailedPage} />
+          <Route path="/settings" component={SettingsDashboard} />
+          <Route path="/createEvent" component={EventForm} />
         </Container>
       </Fragment>
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
@@ -8,13 +10,18 @@ import registerServiceWorker from './registerServiceWorker';
 const rootEl = document.getElementById('root');
 
 const render = () => {
-    ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
 };
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    });
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
 
 render();
